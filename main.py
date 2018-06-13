a36c70a021520331011eb89365a6cdd26cc7da21
Sinister-Paradox <alexdejong737@gmail.com>
2018-06-13 16:13:11 -0500

    Edited main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import twitter
+import twitter, json
 
 consumer_key = 'uSSjBIphPRxfYEtECzmcmjuCA'
 consumer_secret = 'EqDxoqzO7vc1kvjY05q33lM5CAS1E2CNnTn4cjeirMKIJNe74S'
@@ -12,19 +12,22 @@ api = twitter.Api(consumer_key=consumer_key,
                   access_token_key=access_token,
                   access_token_secret=access_token_secret)
 
-ids = set([]) #set of all ids w/ no duplicates
+urls = set([]) #set of all ids w/ no duplicates
 queries = open(query_list, "r")
 for q in queries:
     search = api.GetSearch(q, count=50)
     for tweet in search:
         try:
-            print("https://twitter.com/" + tweet.user.name.replace(" ","")+ "/status/"+str(tweet.id))
-	    x.add(tweet.id) 
+            url = "https://twitter.com/" + tweet.user.screen_name+"/status/"+str(tweet.id)
+            urls.add(url)
         except UnicodeEncodeError:
-            print('Unable to print')
+            print("Could not add")
+		
+def removeResponded():
+    respondedIDs = open(responded, "r")
+    for tempID in respondedIDs:
+        ids.remove(tempID)
 
-def removeResponded(): #removes tweets that have already been responded to
-	respondedIDs = open(responded, "r")
-	for tempID in respondedIDs:
-		ids.remove(tempID)
-
+j = "{\"urls\": [\"" +  "\", \"".join(urls) + "\"]}"
+print(j)
+j = json.loads(j)
