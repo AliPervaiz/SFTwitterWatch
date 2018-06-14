2009b46295bbf868eca060f1279ddf7d8ae12bb8
Jess <jessdejong843@gmail.com>
2018-06-14 01:16:43 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template, url_for, request
 import main
+
+# IBM WATSON
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
+import twitter_wrapper
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+        username='a058b551-a067-48ad-9c08-3d57cb331a12',
+        password='nW4Q8XpczFc4',
+        version='2017-02-27')
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,6 +30,28 @@ def tweets():
         return main.getTweets()
     else:
         return main.getTweets()
+
+@app.route('/sentiment/<message>', methods=['GET'])
+def sentiment(message):
+    response = natural_language_understanding.analyze(
+
+            text = message,
+
+            features = Features(
+                keywords=KeywordsOptions(
+                    emotion=True,
+                    sentiment=True,
+                    limit=1)),
+                
+            language = 'en')
+
+    try:
+        data = json.loads(json.dumps(response, indent=2))
+    except:
+        pass
+
+
+    return data['keywords'][0]['sentiment']['score']
 
 
 
