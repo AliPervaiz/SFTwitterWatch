b2c8328c9017c7097679e6c5d776d7749bea3670
Jess <jessdejong843@gmail.com>
2018-06-13 20:35:24 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+import main
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+# get data?
+@app.route('/tweets', methods=['GET'])
+def tweets():
+    if request.method == 'GET':
+        return main.getTweets()
+    else:
+        return main.getTweets()
+
+
 
 if __name__ == "__main__":
     app.run()
